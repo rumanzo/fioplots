76347e90d99f49717d4e268500a28bb1d02a6a30
Alexey Kostin <akostin@fintech.ru>
2019-01-14 18:16:14 +0300

    Asyncio release

--- logprepare.py
+++ logprepare.py
@@ -3,6 +3,9 @@ import glob
 import argparse
 import sys
 import os
+import asyncio
+import concurrent.futures
+
 
 class Logprepare(object):
     def __init__(self):
@@ -10,49 +13,32 @@ class Logprepare(object):
 
     def logprepare(self, src, dst):
         with open(src, 'r') as srcfile, open(dst, 'w') as dstfile:
-            rows = list(srcfile)
-            rowslen = len(rows)
             second = 1
             valsum = []
-            for num, line in enumerate(rows):
+            row = []
+            for line in srcfile:
                 row = line.rstrip('\n').split(', ')  # [ms, value, type, bs]
-                if num == rowslen:
-                    valsum.append(int(row[1]))
-                    dstfile.write(f'{self.seconds_in_ms * second}, {sum(valsum)}, {row[2]}, {row[3]}\n')
-                elif int(row[0]) < self.seconds_in_ms * second + 1:
-                    valsum.append(int(row[1]))
-                else:
-                    dstfile.write(f'{self.seconds_in_ms * second}, {sum(valsum)}, {row[2]}, {row[3]}\n')
-                    second += 1
-                    valsum.clear()
-                    valsum.append(int(row[1]))
-        return True
-
-    def getparsedlog(self, src):
-        newrows = []
-        with open(src, 'r') as srcfile:
-            rows = list(srcfile)
-            rowslen = len(rows)
-            second = 1
-            valsum = []
-            for num, line in enumerate(rows):
-                row = line.rstrip('\n').split(', ')  # [ms, value, type, bs]
-                if num == rowslen:
-                    valsum.append(int(row[1]))
-                    newrows.append([self.seconds_in_ms * second, sum(valsum), row[2], row[3]])
                 if int(row[0]) < self.seconds_in_ms * second + 1:
                     valsum.append(int(row[1]))
                 else:
-                    newrows.append([self.seconds_in_ms * second, sum(valsum), row[2], row[3]])
+                    dstfile.write(f'{self.seconds_in_ms * second}, {sum(valsum)}, {row[2]}, {row[3]}\n')
                     second += 1
                     valsum.clear()
                     valsum.append(int(row[1]))
-        return newrows
+            dstfile.write(f'{self.seconds_in_ms * second}, {sum(valsum)}, {row[2]}, {row[3]}\n')
+        return f'{src} processed'
 
 
 
-
-
+    async def asynclogprepare(self, executor, args):
+        loop = asyncio.get_event_loop()
+        blocking_tasks = [
+            loop.run_in_executor(executor, self.logprepare, log["src"], log["dst"])
+            for log in args
+        ]
+        completed, pending = await asyncio.wait(blocking_tasks)
+        results = [t.result() for t in completed]
+        print('results: {!r}'.format(results))
 
 
 if __name__ == "__main__":
@@ -67,8 +53,16 @@ if __name__ == "__main__":
     if args.sourcedir == args.destdir:
         print("Source direcroty must be different from destination directory. Exiting")
         sys.exit(1)
+
     logp = Logprepare()
-    print(glob.glob(os.path.join(args.sourcedir, args.pattern)))
+    event_loop = asyncio.get_event_loop()
+    executor = concurrent.futures.ThreadPoolExecutor()
+
+    task_list = []
     for file in glob.glob(os.path.join(args.sourcedir, args.pattern)):
-        print(file, os.path.join(args.destdir, os.path.basename(file)))
-        logp.logprepare(file, os.path.join(args.destdir, os.path.basename(file)))
+        print(f'Processing {file} to {os.path.join(args.destdir, os.path.basename(file))}')
+        task_list.append({"src": file, "dst": os.path.join(args.destdir, os.path.basename(file))})
+    try:
+         event_loop.run_until_complete(logp.asynclogprepare(executor, task_list))
+    finally:
+        event_loop.close()
